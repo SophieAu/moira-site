7cdba3317ec4789303d8a8efab1b46222055e4f2
Sophie Au <SophieAu@users.noreply.github.com>
2020-02-23 19:24:27 +0100

    Add global sidebar

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
+import styles from './Layout.module.css';
 import SEO from './SEO';
+import Sidebar from './Sidebar';
 
 const webpSupportDetection =
   '!function(e){"use strict";function s(s){if(s){var t=e.documentElement;t.classList?t.classList.add("webp"):t.className+=" webp",window.sessionStorage.setItem("webpSupport",!0)}}!function(e){if(window.sessionStorage&&window.sessionStorage.getItem("webpSupport"))s(!0);else{var t=new Image;t.onload=t.onerror=function(){e(2===t.height)},t.src="data:image/webp;base64,UklGRi4AAABXRUJQVlA4TCEAAAAvAUAAEB8wAiMwAgSSNtse/cXjxyCCmrYNWPwmHRH9jwMA"}}(s)}(document);';
@@ -19,7 +21,10 @@ const Layout: React.FC<Props> = ({ title, description, slug, additionalHead, chi
       <script>{webpSupportDetection}</script>
       {additionalHead}
     </SEO>
-    {children}
+    <div id={styles.body}>
+      <Sidebar />
+      {children}
+    </div>
   </>
 );
 
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { Home, SubPages } from '../../data/strings';
+import Link from '../components/Link';
+import styles from './Sidebar.module.css';
+
+export default () => (
+  <header className={styles.root}>
+    <h1>
+      <Link to={Home.path}>Moira Barrett</Link>
+    </h1>
+    <Nav />
+  </header>
+);
+
+const Nav = () => (
+  <nav>
+    <ul className={styles.linkList}>
+      {SubPages.map(page => (
+        <li className={styles.navItem} key={page.slug}>
+          <Link to={page.path}>{page.title}</Link>
+        </li>
+      ))}
+    </ul>
+  </nav>
+);
