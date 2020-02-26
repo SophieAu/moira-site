22535e9d70946ac76ec7652696436c0e81debea7
Sophie Au <SophieAu@users.noreply.github.com>
2020-02-26 09:24:52 +0100

    Recover config file

---
+++ data/config.ts
@@ -0,0 +1,19 @@
+export const slugs = {
+  collage: 'collage',
+  contact: 'contact',
+  home: '',
+  translation: 'translation',
+  writing: 'writing',
+};
+
+export const paths = {
+  collage: `/${slugs.collage}`,
+  contact: `/${slugs.contact}`,
+  home: `/`,
+  translation: `/${slugs.translation}`,
+  writing: `/${slugs.writing}`,
+};
+
+export const BASE_URL = 'https://moira-barrett.com';
+
+export const EMAIL = 'moira@moira-barrett.com';
