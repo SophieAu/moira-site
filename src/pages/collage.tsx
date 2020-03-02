e7ea3198f25fe9680d5470ff339e2451bf4d2df6
Sophie Au <SophieAu@users.noreply.github.com>
2020-03-02 22:14:29 +0100

    First rough collage implementation

--- src/pages/collage.tsx
+++ src/pages/collage.tsx
@@ -1,9 +1,37 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React, { useState } from 'react';
 
-import { Collage } from '../../data/strings';
+import { Collage as strings } from '../../data/strings';
 import Layout from '../components/Layout';
 import styles from './collage.module.css';
 
-export default () => (
-  <Layout title={Collage.pageTitle} description={Collage.description} slug={Collage.slug}></Layout>
-);
+const Collage = () => {
+  const [selectedImage, selectImage] = useState('');
+
+  return (
+    <Layout
+      title={strings.pageTitle}
+      description={strings.description}
+      slug={strings.slug}
+      className={styles.merp}
+    >
+      <img src={imgOne} alt="rubbish" onClick={() => selectImage(imgOne)} />
+      <img src={imgTwo} alt="rubbish" onClick={() => selectImage(imgTwo)} />
+      <img src={imgThree} alt="rubbish" onClick={() => selectImage(imgThree)} />
+      <img src={imgFour} alt="rubbish" onClick={() => selectImage(imgFour)} />
+      <img src={imgFive} alt="rubbish" onClick={() => selectImage(imgFive)} />
+      <img src={imgSix} alt="rubbish" onClick={() => selectImage(imgSix)} />
+      <img src={imgSeven} alt="rubbish" onClick={() => selectImage(imgSeven)} />
+      <img src={imgEight} alt="rubbish" onClick={() => selectImage(imgEight)} />
+      {selectedImage && (
+        <div className={styles.fullSize} onClick={() => selectImage('')}>
+          <img src={selectedImage} alt="rubbish" />
+        </div>
+      )}
+    </Layout>
+  );
+};
+
+export default Collage;
