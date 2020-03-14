2ccfb7962c6fc83fdc14cf7381c1127885cd09f4
Sophie Au <SophieAu@users.noreply.github.com>
2020-03-14 19:28:21 +0100

    Minor refactor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import { Home as strings } from '../../data/strings';
 import Layout from '../components/Layout';
 
 const Home = () => (
-  <Layout title={strings.pageTitle} description={strings.description} slug={strings.slug}></Layout>
+  <Layout title={strings.pageTitle} description={strings.description} slug={strings.slug} />
 );
 
 export default Home;
