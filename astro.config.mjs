e9930b7b697419de85e81a2f78c3e83caa8fb38a
Sophie Au <SophieAu@users.noreply.github.com>
2024-04-19 15:49:48 -0700

    Start setting up cloudinary

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
-import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
+import { defineConfig, passthroughImageService } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
-import robotsTxt from "astro-robots-txt";
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://moira-barrett.com",
-  integrations: [sitemap(), robotsTxt()]
+  site: 'https://moira-barrett.com',
+  integrations: [sitemap(), robotsTxt()],
+  image: { domains: ['res.cloudinary.com'], service: passthroughImageService },
 });
