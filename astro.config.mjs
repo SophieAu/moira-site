e9fd9298b537feb8d0ebdbe6a716eb5f90ccee33
Sophie Au <SophieAu@users.noreply.github.com>
2025-04-25 19:22:38 +0200

    Update deps and go to pnpm

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,40 @@
-import { defineConfig, passthroughImageService } from 'astro/config';
+import { defineConfig, passthroughImageService, fontProviders } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 import robotsTxt from 'astro-robots-txt';
 
+import svelte from '@astrojs/svelte';
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://moira-barrett.com',
-  integrations: [sitemap(), robotsTxt()],
-  image: { domains: ['res.cloudinary.com'], service: passthroughImageService() },
+	site: 'https://moira-barrett.com',
+	integrations: [sitemap(), robotsTxt(), svelte()],
+	image: { domains: ['res.cloudinary.com'], service: passthroughImageService() },
+	experimental: {
+		fonts: [
+			{
+				provider: fontProviders.google(),
+				name: 'Italiana',
+				cssVariable: '--font-italiana',
+			},
+			{
+				provider: 'local',
+				name: 'Fanwood Text',
+				cssVariable: '--font-fanwood',
+				variants: [
+					{
+						style: 'normal',
+						weight: 400,
+						src: ['./public/fonts/Fanwood Text.otf'],
+					},
+					{
+						style: 'italic',
+						weight: 400,
+						src: ['./public/fonts/Fanwood Text Italic.otf'],
+					},
+					// ...
+				],
+			},
+		],
+	},
 });
